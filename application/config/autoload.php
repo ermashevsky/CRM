6d62dfc714138185b951b7b841a757d364185592
Denic <ermashevsky@gmail.com>
2014-10-31 09:47:05 +0300

    Тест

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database','session','ion_auth');
+$autoload['libraries'] = array('session','ion_auth');
 
 /*
 | -------------------------------------------------------------------
