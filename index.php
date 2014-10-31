c341b2e08198e818f4bb6a814081d6efa7fd3898
Denic <ermashevsky@gmail.com>
2014-10-31 10:01:34 +0300

    Тест

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-	define('ENVIRONMENT', 'test');
+	define('ENVIRONMENT', $_SERVER['HTTP_HOST']);
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
